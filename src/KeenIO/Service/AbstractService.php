e38e572cf27f797804775ee8b0f1039752d12182
Tom Anderson <tom.h.anderson@gmail.com>
2012-12-21 16:06:33 -0800

    Cleaned up abstract

--- src/KeenIO/Service/AbstractService.php
+++ src/KeenIO/Service/AbstractService.php
@@ -2,11 +2,6 @@
 
 namespace KeenIO\Service;
 
-use Zend\ServiceManager\ServiceManager;
-
-use Zend\EventManager\EventManager;
-use Zend\EventManager\StaticEventManager;
-
 use Zend\Http\Client;
 use Zend\Http\Headers;
 use Zend\Json\Json;
@@ -15,31 +10,12 @@ use Zend\I18n\Validator\Alnum as Alnum;
 
 abstract class AbstractService {
 
-    private $eventManager;
-    private $projectId;
     private $apiKey;
     private $name;
 
     public function __construct($apiKey)
     {
         $this->setApiKey($apiKey);
-    }
-
-    public function getEventManager()
-    {
-        if (!$this->eventManager instanceof Events) {
-            $this->setEventManager(new EventManager(array(
-                __CLASS__,
-                get_called_class(),
-            )));
-        }
-        return $this->eventManager;
-    }
-
-    public function setEventManager(EventManager $eventManager)
-    {
-        $this->eventManager = $eventManager;
-        return $this;
     }
 
     public function getApiKey()
@@ -60,15 +36,12 @@ abstract class AbstractService {
 
     public function setName($name)
     {
+        $validator = new Alnum();
+        if (!$validator->isValid($name))
+            throw new \Exception("Keen IO name '$name' contains invalid characters or spaces.");
+
         $this->name = $name;
         return $this;
-    }
-
-    protected function verifyCollectionName($collectionName)
-    {
-        $validator = new Alnum();
-        if (!$validator->isValid($collectionName))
-            throw new \Exception("Collection name '$collection' contains invalid characters or spaces.");
     }
 
     protected function validateOptions($allowed, $options)
@@ -94,13 +67,5 @@ abstract class AbstractService {
     protected function sendHttpRequest(Client $http) {
         $response = $http->send();
         return Json::decode($response->getBody());
-    }
-
-    public function projects() {
-        $http = $this->getHttpClient();
-        $http->setUri('https://api.keen.io/3.0/projects');
-        $http->getMethod('GET');
-
-        return $this->sendHttpRequest($http);
     }
 }
