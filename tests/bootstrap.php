80ee7bcebc9294c8ce23d1542712a3a1036a612d
Aaron Scherer <aequasi@gmail.com>
2014-03-05 08:41:57 -0800

    Adding composer self-update, and fixing tests (no autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,8 @@
 error_reporting(-1);
 
 // Ensure that composer has installed all dependencies
-if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
-    die("Dependencies must be installed using composer:\n\nphp composer.phar install\n\n"
+if (!@require dirname(__DIR__) . '/vendor/autoload.php') {
+    die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
         . "See http://getcomposer.org for help with installing composer\n");
 }
 
