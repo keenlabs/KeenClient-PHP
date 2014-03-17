8726a8cbb48db2986ca8976e25a066ee99c66ee6
Aaron Scherer <aequasi@gmail.com>
2014-03-17 09:06:18 -0700

    Removing --dev arg

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ error_reporting(-1);
 
 // Ensure that composer has installed all dependencies
 if (!@require dirname(__DIR__) . '/vendor/autoload.php') {
-    die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
+    die("Dependencies must be installed using composer:\n\nphp composer.phar install\n\n"
         . "See http://getcomposer.org for help with installing composer\n");
 }
 
