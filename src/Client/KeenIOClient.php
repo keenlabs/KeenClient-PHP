a1e6fa9f15fb3a7a2eb5cdd89e555e639bcbe451
Eliot Flannery <eliot@keen.io>
2016-01-14 17:32:16 -0800

    Crypto fixes to properly handle 64 hex char keys.

--- src/Client/KeenIOClient.php
+++ src/Client/KeenIOClient.php
@@ -278,12 +278,25 @@ class KeenIOClient extends Client
             $options['allowed_operations'] = $allowedOperations;
         }
 
-        $optionsJson = $this->padString(json_encode($options));
+        $apiKey = pack('H*', $masterKey);
+        $blockSize = 16;
+
+        /**
+         * Use the old block size and hex string input if using a legacy master key.
+         * Old block size was 32 bytes and old master key was 32 hex characters in length.
+         */
+
+        if (strlen($masterKey) == 32) {
+            $apiKey = $masterKey;
+            $blockSize = 32;
+        }
+
+        $optionsJson = $this->padString(json_encode($options), $blockSize);
 
         $ivLength = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
         $iv       = mcrypt_create_iv($ivLength, $source);
 
-        $encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $masterKey, $optionsJson, MCRYPT_MODE_CBC, $iv);
+        $encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $apiKey, $optionsJson, MCRYPT_MODE_CBC, $iv);
 
         $ivHex        = bin2hex($iv);
         $encryptedHex = bin2hex($encrypted);
