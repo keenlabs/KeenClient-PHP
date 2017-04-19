30a26b414393e662e422025ce96ea3ad7438aa16
Joe Wegner <joe@wegnerdesign.com>
2017-04-19 16:36:17 -0500

    bump to version 2.5.8

--- src/Client/KeenIOClient.php
+++ src/Client/KeenIOClient.php
@@ -40,7 +40,7 @@ use KeenIO\Exception\RuntimeException;
 class KeenIOClient extends GuzzleClient
 {
 
-    const VERSION = '2.5.7';
+    const VERSION = '2.5.8';
 
     /**
      * Factory to create new KeenIOClient instance.
