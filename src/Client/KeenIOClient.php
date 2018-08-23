24c492818a516696928ceaf4c77868730a7a0eaf
Lance Bailey <bailz777@gmail.com>
2018-08-23 12:11:45 +0200

    - Bumped KeenIOClient Version  - Added comments in CHANGE.md about new version fixes
    Signed-off-by: Lance Bailey <bailz777@gmail.com>

--- src/Client/KeenIOClient.php
+++ src/Client/KeenIOClient.php
@@ -41,7 +41,7 @@ use KeenIO\Exception\RuntimeException;
 class KeenIOClient extends GuzzleClient
 {
 
-    const VERSION = '2.5.12';
+    const VERSION = '2.5.13';
 
     /**
      * Factory to create new KeenIOClient instance.
